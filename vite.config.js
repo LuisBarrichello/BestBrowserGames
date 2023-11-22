d6f263dd3dcd1308b5503dd06ed29098f66fb758
Luís Gabriel Viana Barrichello <luisgbarrichelo@gmail.com>
2023-11-22 19:20:20 -0300

    fix: corrigindo bug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import css from 'rollup-plugin-css-only'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), css()],
+  plugins: [react()],
 })
